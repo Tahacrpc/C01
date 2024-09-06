0b8e44648986d67293569296cdd8492ad453d4f5
Tahacrpc <tahacirpici3@gmail.com>
2024-09-06 06:58:15 +0300

    Add files via upload

---
+++ C01/ex07/ft_rev_int_tab.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_rev_int_tab.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tcirpici <tcirpici@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/20 14:38:47 by tcirpici          #+#    #+#             */
+/*   Updated: 2024/08/20 17:12:49 by tcirpici         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <unistd.h>
+
+void	ft_rev_int_tab(int *tab, int size)
+{
+	int	i;
+	int	swap;
+
+	i = 0;
+	while (i < (size / 2))
+	{
+		swap = tab[i];
+		tab[i] = tab[size - 1 - i];
+		tab[size - 1 - i] = swap ;
+		i++;
+	}
+}
---
+++ C01/ex08/ft_sort_int_tab.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sort_int_tab.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tcirpici <tcirpici@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/20 15:18:43 by tcirpici          #+#    #+#             */
+/*   Updated: 2024/08/20 17:08:33 by tcirpici         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <unistd.h>
+
+void	ft_sort_int_tab(int *tab, int size)
+{
+	int	tmp;
+	int	i;
+
+	while (size >= 1)
+	{
+		i = 0;
+		while (i < (size - 1))
+		{
+			if (tab[i] > tab[i + 1])
+			{
+				tmp = tab[i];
+				tab[i] = tab[i + 1];
+				tab[i + 1] = tmp;
+			}
+			i++;
+		}
+		size--;
+	}
+}
